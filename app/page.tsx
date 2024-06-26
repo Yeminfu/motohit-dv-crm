780c4acb4c28404cc35ba2b40ac840b01dbc930c
Yeminfu <yeminfu4319@gmail.com>
2024-06-27 00:12:51 +1000

    скрыл миграцию

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import AuthedLayout from "@/utils/authedLayout";
-import StartMigration from "../tools/devtools/migrate/startMigration";
+// import StartMigration from "../tools/devtools/migrate/startMigration";
 
 export default function Home() {
   return (
     <main>
       <AuthedLayout title="Главная">
-        <StartMigration />
+        <>
+          {/* <StartMigration /> */}
+        </>
       </AuthedLayout>
     </main>
   );
