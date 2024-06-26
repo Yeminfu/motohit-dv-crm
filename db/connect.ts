355a447a0bdd5cd925cbcc9cfdf7296699a7e464
Yeminfu <yeminfu4319@gmail.com>
2024-06-27 00:01:31 +1000

    fix db connect

--- db/connect.ts
+++ db/connect.ts
@@ -3,10 +3,8 @@ import mysql from "mysql2/promise";
 export default async function dbConnection() {
   return await mysql.createConnection({
     host: "localhost",
-    user: "admin",
-    password: "xKd9kF2eA",
-    database: "motohit_dv_crm",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
 }
-
-// export default dbCOnnection;
