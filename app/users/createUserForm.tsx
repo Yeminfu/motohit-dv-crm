617ea6c42ea67586cd35460c1ff3dd4348033169
Yeminfu <yeminfu4319@gmail.com>
2024-07-09 11:58:05 +1000

    сделал форму создаия пользователей

---
+++ app/users/createUserForm.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import { ShopFromDB } from "@/types/shops/shopFromDBType";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+
+export default function CreateUserForm(props: { shops: ShopFromDB[] }) {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        control,
+        formState: { errors },
+    } = useForm()
+    return <>
+        <h3>Создать пользователя</h3>
+        {/* {JSON.stringify(props.shops)} */}
+        <form onSubmit={handleSubmit(async x => {
+            await onSubmit(x);
+        })}>
+            <div className="row">
+                <div className="col">
+                    <div className="mb-2">
+                        <div><b>Имя</b></div>
+                        <input {...register("name", { required: true })} placeholder="" className="form-control" autoComplete="off" />
+                    </div>
+                    <div className="mb-2">
+                        <div><b>Телеграм (без @!)</b></div>
+                        <input {...register("telegram_username", { required: true })} placeholder="" className="form-control" autoComplete="off" />
+                    </div>
+                    <div>
+                        <button className="btn-primary">Сохранить</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </>
+}
+
+async function onSubmit(values: any) {
+    const { name, telegram_username } = values;
+    if( /@/.test( telegram_username)){
+        toast.error('Ошибка! В телеграме не должно быть "@"')
+        return;
+    }
+    console.log('values', values);
+    fetch('api/users/create', {
+        method: 'post',
+        body: JSON.stringify({
+            name,
+            telegram_username
+        })
+    })
+}
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,12 +1,16 @@
 import AuthedLayout from "@/utils/authedLayout";
 import getAllUsers from "./getAllUsers";
+import CreateUserForm from "./createUserForm";
+import getShops from "../category/[id]/getShops";
 
 export default async function Page() {
     const users = await getAllUsers();
+    const shops = await getShops()
     return <>
         <AuthedLayout title={"Пользователи"}>
             <>
                 <pre>{JSON.stringify(users, null, 2)}</pre>
+                <CreateUserForm shops={shops}/>
             </>
         </AuthedLayout>
     </>
