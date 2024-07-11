0a01a17384541ed742308369224e192680b21575
Yeminfu <yeminfu4319@gmail.com>
2024-07-12 08:34:28 +1000

    вывел таблицу историей

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,12 +1,23 @@
 import dbConnection from "@/db/connect";
 import AuthedLayout from "@/utils/authedLayout";
+import dayjs from "dayjs";
 
 export default async function Page() {
     const history = await getHistory();
     return <>
         <AuthedLayout title="История">
             <>
-                <pre>{JSON.stringify(history, null, 2)}</pre>
+                <table className="table table-bordered table-striped">
+                    <tbody>
+                        {history.map(historyRow => <tr key={historyRow.id}>
+                            <td>{historyRow.id}</td>
+                            <td>{dayjs(historyRow.created_date).toString()}</td>
+                            <td><pre>{JSON.stringify(historyRow.data, null, 2)}</pre></td>
+                            <td>{historyRow.doneBy}  </td>
+                        </tr>)}
+                    </tbody>
+                </table>
+                {/* <pre>{JSON.stringify(history, null, 2)}</pre> */}
             </>
         </AuthedLayout>
     </>
