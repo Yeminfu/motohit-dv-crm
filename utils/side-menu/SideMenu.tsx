25a72b988a4aca2f4734f2c64a48027b7ae176f6
Yeminfu <yeminfu4319@gmail.com>
2024-07-21 11:46:37 +1000

    проверка на босса в меню

--- utils/side-menu/SideMenu.tsx
+++ utils/side-menu/SideMenu.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import CreateCategory from "../createCategory/createCategory";
 import getAllCategories from "../getAllCategories";
+import { cookies } from "next/headers";
+import getUserByToken from "../users/getUserByToken";
 
 export default async function SideMenu() {
+    const authToken = String(cookies().get("auth")?.value);
+    const user = await getUserByToken(authToken);
     const categories = await getAllCategories();
     return <>
         <div className="mb-2">
@@ -16,9 +20,13 @@ export default async function SideMenu() {
         <div className="mt-4">
             <Link className="btn btn-dark d-block text-start mb-1" href={`/users`}>Пользователи</Link>
         </div>
-        <div className="mt-4">
-            <Link className="btn btn-dark d-block text-start mb-1" href={`/sum-in-product`}>Сумма в товаре</Link>
-        </div>
+        {
+            (user?.is_boss)
+                ? <div className="mt-4">
+                    <Link className="btn btn-dark d-block text-start mb-1" href={`/sum-in-product`}>Сумма в товаре</Link>
+                </div>
+                : null
+        }
     </>
 }
 
