eb30af88636327fdf2fa675ca4a3d1db9e71e7f9
Yeminfu <yeminfu4319@gmail.com>
2024-07-23 11:02:07 +1000

    rm gb

--- utils/getProductsFull.ts
+++ utils/getProductsFull.ts
@@ -3,13 +3,11 @@ import getProductImages from "./getProductImages";
 import getProductRetailPrices from "./getProductRetailPrices";
 import getProductStock from "./getProductStock";
 import getProductsByCategoryId from "./getProductsByCategoryId";
-import getShops from "./getShops";
 
 export default async function getProductsFull(
   idCategory: number
 ): Promise<ProductsFull[]> {
   const products = await getProductsByCategoryId(idCategory);
-  const shops = await getShops();
   const productsFull: ProductsFull[] = await Promise.all(
     products.map(async (product) => {
       const images = await getProductImages(product.id);
