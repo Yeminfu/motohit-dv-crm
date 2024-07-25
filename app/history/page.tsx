b848dd0f2d1c3b50f37d687c949f2fc9a0409aa6
Yeminfu <yeminfu4319@gmail.com>
2024-07-25 13:15:25 +1000

    history order by id desc

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -3,43 +3,48 @@ import AuthedLayout from "@/utils/authedLayout";
 import dayjs from "dayjs";
 
 export default async function Page() {
-    const history = await getHistory();
-    return <>
-        <AuthedLayout title="История">
-            <>
-                <table className="table table-bordered table-striped">
-                    <tbody>
-                        {history.map(historyRow => <tr key={historyRow.id}>
-                            <td>{historyRow.id}</td>
-                            <td>{historyRow.action}</td>
-                            <td>{historyRow.doneBy}  </td>
-                            <td>{dayjs(historyRow.created_date).toString()}</td>
-                            <td>
-                                <div>{typeof historyRow.data}</div>
-                                <pre>{JSON.stringify(historyRow.data, null, 2)}</pre></td>
-                        </tr>)}
-                    </tbody>
-                </table>
-                {/* <pre>{JSON.stringify(history, null, 2)}</pre> */}
-            </>
-        </AuthedLayout>
-    </>
+  const history = await getHistory();
+  return <>
+    <AuthedLayout title="История">
+      <>
+        <table className="table table-bordered table-striped">
+          <tbody>
+            {history.map(historyRow => <tr key={historyRow.id}>
+              <td>{historyRow.id}</td>
+              <td>{historyRow.action}</td>
+              <td>{historyRow.doneBy}  </td>
+              <td>{dayjs(historyRow.created_date).toString()}</td>
+              <td>
+                <div>{typeof historyRow.data}</div>
+                <pre>{JSON.stringify(historyRow.data, null, 2)}</pre></td>
+            </tr>)}
+          </tbody>
+        </table>
+        {/* <pre>{JSON.stringify(history, null, 2)}</pre> */}
+      </>
+    </AuthedLayout>
+  </>
 }
 
 async function getHistory(): Promise<ts_historyInDB[]> {
-    const connection = await dbConnection();
-    const res: any = await connection.query(`select * from ${process.env.TABLE_PREFIX}_history`).then(([x]: any) => x.map((x: any) => ({
-        ...x,
-        data: typeof x.data === 'object' ? x : JSON.parse(x.data)
-    })));
-    await connection.end();
-    return res
+  const connection = await dbConnection();
+  const res: any = await connection.query(`
+        select 
+          *
+        from ${process.env.TABLE_PREFIX}_history
+        order by id desc
+    `).then(([x]: any) => x.map((x: any) => ({
+    ...x,
+    data: typeof x.data === 'object' ? x : JSON.parse(x.data)
+  })));
+  await connection.end();
+  return res
 }
 
 interface ts_historyInDB {
-    id: number
-    created_date: string
-    data: string
-    action: string
-    doneBy: number
+  id: number
+  created_date: string
+  data: string
+  action: string
+  doneBy: number
 }
