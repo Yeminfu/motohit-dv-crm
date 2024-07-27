675e7eb5b6dfdc1b3598ef1058d4c28d244ac36a
Yeminfu <yeminfu4319@gmail.com>
2024-07-27 12:11:47 +1000

    создал форму в фильтре

--- app/category/[id]/filter.tsx
+++ app/category/[id]/filter.tsx
@@ -1,5 +1,38 @@
+import { useForm } from "react-hook-form";
+
+interface ts_inputs {
+    name: string
+}
+
 export default function Filter() {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        control,
+        formState: { errors },
+    } = useForm<ts_inputs>({
+
+        defaultValues: {
+            // "name": props.product.name,
+        }
+    });
+
+
     return <>
-        filter
+        <div className="my-3">
+            <div className="shadow p-2">
+                <div><strong>Фильтр</strong></div>
+
+                <form onSubmit={x => {
+                    console.log('x', x);
+                }}>
+                    <div className="d-flex">
+                        <input {...register("name", { required: true })} placeholder="Наименование/код" className="form-control w-auto" autoComplete="off" />
+                        <button className="btn btn-dark ms-2 btn-sm" >Фильтр</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </>
 }
