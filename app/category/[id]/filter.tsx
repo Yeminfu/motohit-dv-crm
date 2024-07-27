2926d159be4244f5bd30e4387d02263d164f44a2
Yeminfu <yeminfu4319@gmail.com>
2024-07-27 12:21:28 +1000

    внедрил pathname

--- app/category/[id]/filter.tsx
+++ app/category/[id]/filter.tsx
@@ -1,10 +1,20 @@
 import { useForm } from "react-hook-form";
+import { useRouter } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 
 interface ts_inputs {
     name: string
 }
-
 export default function Filter() {
+    const router = useRouter()
+
+    const pathname = usePathname()
+
+    async function onSubmit(x: any) {
+        console.log('x', x);
+        router.push(`${pathname}?name=${x.name}`)
+    }
+
     const {
         register,
         handleSubmit,
@@ -24,11 +34,9 @@ export default function Filter() {
             <div className="shadow p-2">
                 <div><strong>Фильтр</strong></div>
 
-                <form onSubmit={x => {
-                    console.log('x', x);
-                }}>
+                <form onSubmit={handleSubmit(onSubmit)}                >
                     <div className="d-flex">
-                        <input {...register("name", { required: true })} placeholder="Наименование/код" className="form-control w-auto" autoComplete="off" />
+                        <input {...register("name")} placeholder="Наименование/код" className="form-control w-auto" autoComplete="off" />
                         <button className="btn btn-dark ms-2 btn-sm" >Фильтр</button>
                     </div>
                 </form>
