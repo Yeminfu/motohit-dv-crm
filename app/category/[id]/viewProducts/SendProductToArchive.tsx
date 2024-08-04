f0c9721b28b579656e2afb81b6e1dac1043684f4
Yeminfu <yeminfu4319@gmail.com>
2024-08-04 11:05:30 +1000

    уведомление при удалении товара

--- app/category/[id]/viewProducts/SendProductToArchive.tsx
+++ app/category/[id]/viewProducts/SendProductToArchive.tsx
@@ -1,5 +1,6 @@
 import Modal from "@/utils/modal/modal";
 import { useState } from "react";
+import { toast } from "react-toastify";
 
 export default function SendProductToArchive(props: { idProduct: number, productName: string }) {
     const [isOpen, setIsOpen] = useState(false);
@@ -8,7 +9,9 @@ export default function SendProductToArchive(props: { idProduct: number, product
             setIsOpen(false);
         }}>
             <>
-                <button className="btn btn-sm btn-danger" onClick={() => onConfirm(props.idProduct)}>Подтвердить</button>
+                <button className="btn btn-sm btn-danger" onClick={() => onConfirm(props.idProduct, () => {
+                    setIsOpen(false)
+                })}>Подтвердить</button>
                 <button className="btn btn-sm btn-dark ms-2" onClick={() => {
                     setIsOpen(false)
                 }}>Отмена</button>
@@ -23,13 +26,18 @@ export default function SendProductToArchive(props: { idProduct: number, product
     </>
 }
 
-async function onConfirm(idProduct: number) {
-    // alert(idProduct)
+async function onConfirm(idProduct: number, closeFn: any) {
     fetch(
         `/api/products/send-to-archive/${idProduct}`, { method: "post" }
     )
         .then(x => x.json())
         .then(x => {
             console.log('xxxxxxx', x);
+            if (x.success) {
+                closeFn();
+                toast.success('Товар отправлен в архив')
+            } else {
+                toast.error('Ошибка #f93m')
+            }
         })
 }
