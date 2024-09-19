b5c7d4b8e1992b44a0ba7e67c10408a220bcb325
Yeminfu <yeminfu4319@gmail.com>
2024-09-19 21:09:52 +1000

    up

--- app/products/create/page.tsx
+++ app/products/create/page.tsx
@@ -1,18 +1,17 @@
 import getAllCategoriesWithProducts from "@/tools/db/getAllCategoriesWithProducts";
 import CreateProductForm from "./form"
 import { CategoryFromDBInterface } from "@/types/categories/categories";
-// import getAllCategoriesWithProducts from "@/src/components/db/getAllCategoriesWithProducts";
-// import { CategoryFromDBInterface } from "@/src/app/types/categories";
+import AuthedLayout from "@/utils/authedLayout";
 
-// FIXME пути в квадратных кскобках не катят тут и в категориях. надо решить вопрос с кешем этих страниц, не получается убрать кеш на получении категорий
 export const dynamic = 'force-dynamic';
 
 export default async function Page() {
     const categories: CategoryFromDBInterface[] = await getAllCategoriesWithProducts(); //FIXME дубль
     return (
         <>
-            <h1>Создать товар</h1>
-            <CreateProductForm categories={categories} />
+            <AuthedLayout title={"Создать товар"}>
+                <CreateProductForm categories={categories} />
+            </AuthedLayout>
         </>
     )
 }
