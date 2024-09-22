30f3d4e5173bd1d7c0f99aae850889877731a922
Yeminfu <yeminfu4319@gmail.com>
2024-09-22 12:01:59 +1000

    создал createAttributesTable и createAttributesValuesTable

--- app/api/migrate/route.ts
+++ app/api/migrate/route.ts
@@ -12,6 +12,8 @@ import createRetailPricesTable from "./utils/createRetailPricesTable";
 import createStockTable from "./utils/createStockTable";
 import createSuperuser from "./utils/createSuperuser";
 import createHistoryTable from "./utils/createHistoryTable";
+import createAttributesTable from "./utils/createAttributesTable";
+import createAttributesValuesTable from "./utils/createAttributesValuesTable";
 
 export async function GET() {
   await migrate();
@@ -24,12 +26,15 @@ async function migrate() {
   if (process.env.CAN_MIGRATE !== "poopaloopa") return;
   const connection = await dbConnection();
   await connection.query("SET FOREIGN_KEY_CHECKS=0;");
+
   await createUsersTable();
   await createTokensTable();
   await createShopsTable();
   await createCategoriesTable();
+
   await createProductsTable();
   await createProductsImagesTable();
+
   await createPriceTypesTable();
   await createStockTable();
   await createSalesTable();
@@ -38,4 +43,9 @@ async function migrate() {
   await createSuperuser();
   await createRetailPricesTable();
   await createHistoryTable();
+
+  await createAttributesTable();
+  await createAttributesValuesTable();
 }
+
+
---
+++ app/api/migrate/utils/createAttributesTable.ts
@@ -0,0 +1,34 @@
+import dbConnection from "@/db/connect";
+
+const { TABLE_PREFIX } = process.env;
+
+export default async function createAttributesTable() {
+    const connection = await dbConnection();
+    await connection.query("SET FOREIGN_KEY_CHECKS=0");
+    await connection.query(`drop table if exists ${TABLE_PREFIX}_attributes`);
+    await connection
+        .query(
+            `
+            CREATE TABLE ${TABLE_PREFIX}_attributes (
+                id int primary key AUTO_INCREMENT,
+                attribute_name varchar(250) not null unique,
+                created_date datetime default CURRENT_TIMESTAMP,
+                created_by int not null,
+                view_in_filter TINYINT(1) default 1,
+                idCategory int not null,
+                is_main int not null,
+                foreign key (idCategory) references ${TABLE_PREFIX}_categories(id),
+                foreign key (created_by) references ${TABLE_PREFIX}_users(id)
+            );
+        `
+        )
+        .then(([x]: any) => {
+            // console.log(`${TABLE_PREFIX}_categories`, x);
+        })
+        .catch((z) => {
+            console.log("err #в0ыл", z);
+        });
+    await connection.query("SET FOREIGN_KEY_CHECKS=1");
+    await connection.end();
+}
+
