d67e7847717767f5612d43414939a53fb10e2146
Yeminfu <yeminfu4319@gmail.com>
2024-09-22 19:20:58 +1000

    создал createPreudoAttributes

--- app/api/migrate/utils/createSubdataFunctions/createSubdataFunctions.ts
+++ app/api/migrate/utils/createSubdataFunctions/createSubdataFunctions.ts
@@ -1,14 +1,7 @@
 import createPseudoCategories from "./createPseudoCategories";
-import createAttribute from "@/db/crud/createAttribute";
+import createPreudoAttributes from "./createPreudoAttributes";
 
 export async function createSubdataFunctions() {
   await createPseudoCategories();
   await createPreudoAttributes();
 }
-
-async function createPreudoAttributes() {
-  await createAttribute(
-    'название', 1, 1, 1, 1
-  )
-}
-
