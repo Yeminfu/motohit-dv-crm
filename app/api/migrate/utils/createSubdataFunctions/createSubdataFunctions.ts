168613f864d29f36302ed1afc54de497794bd230
Yeminfu <yeminfu4319@gmail.com>
2024-09-22 13:41:05 +1000

    SAVE

--- app/api/migrate/utils/createSubdataFunctions/createSubdataFunctions.ts
+++ app/api/migrate/utils/createSubdataFunctions/createSubdataFunctions.ts
@@ -1,9 +1,20 @@
 import createCategory from "@/db/crud/createCategory";
 
 export async function createSubdataFunctions() {
-  const array = Array.from({ length: 10 }, (_, b) => b);
+  const array = Array.from({ length: 3 }, (_, b) => b);
   for (let index = 0; index < array.length; index++) {
     const element = array[index];
-    await createCategory("название" + element, "описание" + element)
+    const categoryId = await createCategory("название" + element, "описание" + element);
+    console.log('categoryId', categoryId.insertId,);
+    // if (Boolean(index % 4)) {
+      const newCategory = await createCategory(
+        "название" + element + categoryId.insertId,
+        "описание" + element + categoryId.insertId,
+        categoryId.insertId
+      );
+      console.log('newCategory', newCategory.insertId);
+
+    // }
+
   }
 }
