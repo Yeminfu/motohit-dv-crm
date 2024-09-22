ada6ac188e333f72f68d75cdb5de00e5745480e4
Yeminfu <yeminfu4319@gmail.com>
2024-09-22 19:05:19 +1000

    создал createAttribute

--- app/api/migrate/utils/createSubdataFunctions/createSubdataFunctions.ts
+++ app/api/migrate/utils/createSubdataFunctions/createSubdataFunctions.ts
@@ -1,5 +1,15 @@
+import getDataFromDB from "@/db/getDataFromDB";
 import createPseudoCategories from "./createPseudoCategories";
+import createAttribute from "@/db/crud/createAttribute";
 
 export async function createSubdataFunctions() {
   await createPseudoCategories();
+  await createPreudoAttributes();
 }
+
+async function createPreudoAttributes() {
+  await createAttribute(
+    'название', 1, 1, 1, 1
+  )
+}
+
---
+++ db/crud/createAttribute.ts
@@ -0,0 +1,35 @@
+import getDataFromDB from "../getDataFromDB";
+
+export default async function createAttribute(
+    attribute_name: string,
+    created_by: number,
+    view_in_filter: boolean | 0 | 1,
+    idCategory: number,
+    is_main: boolean | 0 | 1
+) {
+    const result = await getDataFromDB(`
+    insert into ${process.env.TABLE_PREFIX}_attributes
+    (
+      attribute_name,
+      created_by,
+      view_in_filter,
+      idCategory,
+      is_main
+    )
+    values
+    (
+      ?,
+      ?,
+      ?,
+      ?,
+      ?
+    )
+  `, [
+        attribute_name,
+        created_by,
+        view_in_filter,
+        idCategory,
+        is_main
+    ]);
+    return result;
+}
