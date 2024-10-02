e50b963873c9602c06880372699cfb6f74b127f5
Yeminfu <yeminfu4319@gmail.com>
2024-10-02 21:12:57 +1000

    save

--- app/attributes/category/[id]/page.tsx
+++ app/attributes/category/[id]/page.tsx
@@ -1,4 +1,22 @@
-export default function Page() {
-    return <>
-        page</>
+import dbWorker from "@/db/dbWorker";
+import { AttributeType } from "@/types/categories/attributes";
+import AuthedLayout from "@/utils/authedLayout";
+
+export default async function Page(a: { params: { id: string } }) {
+  const attributes = await getAttributes(Number(a.params.id))
+  return <AuthedLayout title="название категории">
+    page {a.params.id}
+    
+    <pre>{JSON.stringify(attributes, null, 2)}</pre>
+  </AuthedLayout>
+}
+
+async function getAttributes(idCategory: number): Promise<AttributeType> {
+  return await dbWorker(`
+    select
+      *
+    from ${process.env.TABLE_PREFIX}_attributes
+    where
+      idCategory = ?
+  `, [idCategory])
 }
--- types/attributes/attributes.tsx
+++ types/attributes/attributes.tsx
@@ -6,8 +6,8 @@ export default async function Attributes() {
     return <>
         <div className="list-group">
             <div className="list-group-item list-group-item-action list-group-item-dark active">Выберите категорию</div>
-            <div className="list-group-item list-group-item-action">
-            </div>
+            {/* <div className="list-group-item list-group-item-action">
+            </div> */}
             {attributes.map(attribute => <Link href={`/attributes/category/${attribute.id}`} className="list-group-item list-group-item-action">
                 <>
                     {attribute.category_name}
