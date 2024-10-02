b951bbdcb0c5aa6c4a6da97e3b5de7d56e9e1c6e
Yeminfu <yeminfu4319@gmail.com>
2024-10-03 05:54:45 +1000

    вывел  название категории

--- app/attributes/category/[id]/page.tsx
+++ app/attributes/category/[id]/page.tsx
@@ -1,13 +1,19 @@
 import dbWorker from "@/db/dbWorker";
 import { AttributeType } from "@/types/categories/attributes";
+import { CategoryFromDBInterface } from "@/types/categories/categories";
 import AuthedLayout from "@/utils/authedLayout";
 
 export default async function Page(a: { params: { id: string } }) {
-  const attributes = await getAttributes(Number(a.params.id))
-  return <AuthedLayout title="название категории">
+  const category = await getCategory(Number(a.params.id));
+  // console.log('category', category);
+  if (!category) return;
+
+  // const attributes = await getAttributes(Number(a.params.id))
+
+  return <AuthedLayout title={"Атрибуты категории: " + category.category_name}>
     page {a.params.id}
-    
-    <pre>{JSON.stringify(attributes, null, 2)}</pre>
+    <pre>{JSON.stringify(category, null, 2)}</pre>
+    {/* <pre>{JSON.stringify(attributes, null, 2)}</pre> */}
   </AuthedLayout>
 }
 
@@ -19,4 +25,14 @@ async function getAttributes(idCategory: number): Promise<AttributeType> {
     where
       idCategory = ?
   `, [idCategory])
+}
+
+async function getCategory(idCategory: number): Promise<CategoryFromDBInterface | undefined> {
+  return dbWorker(`
+  select
+    *
+  from ${process.env.TABLE_PREFIX}_categories
+  where
+    id = ?
+`, [idCategory]).then(x => x.pop())
 }
