715a0965edb70423a70deca8079959a5452ac80a
Yeminfu <yeminfu4319@gmail.com>
2024-10-02 20:51:39 +1000

    up

--- app/attributes/get/[id]/page.tsx
+++ app/attributes/get/[id]/page.tsx
@@ -1,5 +1,22 @@
+import dbWorker from "@/db/dbWorker";
+import { AttributeType } from "@/types/categories/attributes";
 import AuthedLayout from "@/utils/authedLayout";
 
-export default function Page() {
-    return <AuthedLayout title="название_атрибута">page</AuthedLayout>
+export default async function Page(a: { params: { id: string } }) {
+  const attribute = await getAttributeById(1);
+
+  return <AuthedLayout title={String(attribute?.attribute_name)}>Атрибут № {a.params.id}</AuthedLayout>
+}
+
+async function getAttributeById(idAttribute: number): Promise<AttributeType | undefined> {
+  const attributes = await dbWorker(`
+    select
+      *
+    from
+      ${process.env.TABLE_PREFIX}_attributes
+    where
+      id = ?
+  `, [idAttribute]);
+  if (!attributes.length) return undefined;
+  return attributes.pop();
 }
