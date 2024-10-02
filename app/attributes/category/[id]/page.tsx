637d4ece2b4e2ec18045bb36a47f9dad6b988e7e
Yeminfu <yeminfu4319@gmail.com>
2024-10-03 06:24:44 +1000

    создал attributesWithValues

--- app/attributes/category/[id]/page.tsx
+++ app/attributes/category/[id]/page.tsx
@@ -1,13 +1,21 @@
 import AuthedLayout from "@/utils/authedLayout";
 import getAttributes from "./getAttributes";
 import getCategory from "./getCategory";
+import dbWorker from "@/db/dbWorker";
 
 export default async function Page(a: { params: { id: string } }) {
   const category = await getCategory(Number(a.params.id));
-  // console.log('category', category);
+
   if (!category) return;
 
-  const attributes = await getAttributes(Number(a.params.id))
+  const attributes = await getAttributes(Number(a.params.id));
+
+  const attributesWithValues = await Promise.all(
+    attributes.map(async attribute => ({
+      ...attribute,
+      values: await getAttributeValues(attribute.id)
+    }))
+  )
 
   return <AuthedLayout title={"Атрибуты категории: " + category.category_name}>
     <table className="table table-bordered ">
@@ -16,7 +24,7 @@ export default async function Page(a: { params: { id: string } }) {
           <th>id</th><th>Название атрибута</th><th>Виден в фильтре</th><th>Открыт в фильтре</th><th>Главный</th><th>Значения</th><th></th></tr>
       </thead>
       <tbody>
-        {attributes.map(attribute => <tr key={attribute.id}>
+        {attributesWithValues.map(attribute => <tr key={attribute.id}>
           <td>{attribute.id}</td>
           <td>{attribute.attribute_name}</td>
           <td>{attribute.view_in_filter}</td>
@@ -29,4 +37,20 @@ export default async function Page(a: { params: { id: string } }) {
       </tbody>
     </table>
   </AuthedLayout>
+}
+
+// chbfs_attributes_values
+
+async function getAttributeValues(idAttribute: number) {
+  return dbWorker(`
+    select
+      *
+    from ${process.env.TABLE_PREFIX}_attributes_values
+    where
+      idAttribute = ?
+`, [idAttribute])
+}
+
+interface ts_attributeValue {
+
 }
