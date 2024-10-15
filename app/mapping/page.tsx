932b388985d079128c0a582e1092076df164e1c3
Yeminfu <yeminfu4319@gmail.com>
2024-10-16 00:08:38 +1000

    up

--- app/mapping/page.tsx
+++ app/mapping/page.tsx
@@ -1,56 +1,58 @@
 import AuthedLayout from "@/utils/authedLayout";
-import getPRoductsFromShop from "./getPRoductsFromShop";
-import getMatches from "./getMatches";
-import ts_productFromShopWithMatches from "./ts_productFromShopWithMatches";
+import dbWorker from "@/db/dbWorker";
+import ts_productMatch from "./ts_productMatch";
 
 export default async function Page() {
-  const productsFromShop = await getPRoductsFromShop();
-  const productsWithMatches: ts_productFromShopWithMatches[] = await Promise.all(
-    productsFromShop
-      .map(async (productFromShop) => {
-        const matches = await getMatches(productFromShop.id)
-        return {
-          ...productFromShop,
-          matches: matches
-        }
-      }));
+  const matches = await getMatches();
 
   return <>
     <AuthedLayout title="Mapping">
       <>
-        <table className="table table-bordered striped">
+        <table className="table table-bordered table-striped">
           <thead>
             <tr>
               <th>Товар из магазина</th>
               <th>Совпадения (товары из старой crm)</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
-            {productsWithMatches.map(productFromShop => {
-              return <tr key={productFromShop.id}>
-                <td>{productFromShop.product_name}</td>
-                <td>
-                  <table className="table striped">
-                    <tbody>
-                      {productFromShop.matches.map(match => <tr key={match.id}>
-                        <td>
-                          <div style={{ width: 400 }}>
-                            {match.productFromOldCRM.name}
-                          </div>
-                        </td>
-                        <td>
-                          <button className="btn btn-success">Подтвердить</button>
-                          <button className="btn btn-danger">Отмена</button>
-                        </td>
-                      </tr>)}
-                    </tbody>
-                  </table>
-                </td>
-              </tr>
-            })}
+            {matches.map(product => <tr key={product.idProductFromMapping}>
+              <td>{product.nameFromShop}</td>
+              <td>{product.nameFromOldCrm}</td>
+              <td>
+                <div className="d-flex">
+                  <button className="btn btn-success me-2">Подтвердить</button>
+                  <button className="btn btn-danger">Отмена</button>
+                </div>
+              </td>
+            </tr>)}
           </tbody>
         </table>
       </>
     </AuthedLayout>
   </>
+}
+
+async function getMatches(): Promise<ts_productMatch[]> {
+  const matches = await dbWorker(`
+    select
+      productsFromMapping.id as idProductFromMapping,
+      productsFromMapping.idProductFromShop as idProductFromShop,
+      productsFromMapping.idProductFromOldCrm as idProductFromOldCrm,
+      productsFromOldCRM.name as nameFromOldCrm,
+      productsFromShop.product_name as nameFromShop
+  from motohit_dv_mapping.products as productsFromMapping
+      left join
+          motohit_27_crm.birm_products as productsFromOldCRM 
+      on
+          productsFromOldCRM.id = productsFromMapping.idProductFromOldCrm
+      left join
+          motohit_dv.products as productsFromShop 
+      on
+          productsFromShop.id = productsFromMapping.idProductFromShop
+  `, []);
+  console.log('matches', matches);
+
+  return matches;
 }
---
+++ app/mapping/ts_productMatch.ts
@@ -0,0 +1,7 @@
+export default interface ts_productMatch {
+    idProductFromMapping: number
+    idProductFromShop: string
+    idProductFromOldCrm: string
+    nameFromShop: string
+    nameFromOldCrm: string
+}
