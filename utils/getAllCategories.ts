ee1b122af24c53bec5523d81944f8c6250262926
Yeminfu <yeminfu4319@gmail.com>
2024-10-17 18:17:09 +1000

    up

--- utils/getAllCategories.ts
+++ utils/getAllCategories.ts
@@ -1,9 +1,12 @@
-import dbConnection from "@/db/connect";
+import dbWorker from "@/db/dbWorker";
 import { ts_categoryType } from "@/types/categories/categoryType";
 
 export default async function getAllCategories(): Promise<ts_categoryType[]> {
-    const connection = await dbConnection();
-    const categories: ts_categoryType[] = await connection.query(`select id, category_name from ${process.env.TABLE_PREFIX}_categories`).then(([x]: any) => x);
-    await connection.end();
-    return categories;
+  const categories: ts_categoryType[] = await dbWorker(`
+        select
+            id,
+            category_name 
+        from ${process.env.TABLE_PREFIX}_categories`, [])
+    .then((x: any) => x);
+  return categories;
 }
