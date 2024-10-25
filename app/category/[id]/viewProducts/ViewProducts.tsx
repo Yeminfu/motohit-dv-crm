e760a79a93c39d986307fff92054ea270aab8d5e
Yeminfu <yeminfu4319@gmail.com>
2024-10-26 08:02:51 +1000

    codestyle

--- app/category/[id]/viewProducts/ViewProducts.tsx
+++ app/category/[id]/viewProducts/ViewProducts.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { ProductsFull } from "@/types/products/prodyctType";
 import { ShopFromDB } from "@/types/shops/shopFromDBType";
 import Price from "@/ui/price";
@@ -19,40 +19,59 @@ import Link from "next/link";
 // import tsAttributeWithValues from "@/types/attributes/ts_attributesWithValues";
 
 export default function ViewProducts(props: {
-    productsFull: ProductsFull[],
-    shops: ShopFromDB[],
-    priceTypes: PriceTypesFromDBInterface[],
-    searchParams: ts_categoryFilter
-    
+  productsFull: ProductsFull[];
+  shops: ShopFromDB[];
+  priceTypes: PriceTypesFromDBInterface[];
+  searchParams: ts_categoryFilter;
 }) {
-    const [viewAll, setViewAll] = useState(false);
-    return <>
-        <div className="my-2"><button className="btn btn-outline-dark btn-sm" onClick={() => setViewAll(!viewAll)}>{viewAll ? "Скрыть" : "Показать"} дополнительные поля</button></div>
+  const [viewAll, setViewAll] = useState(false);
+  return (
+    <>
+      <div className="my-2">
+        <button
+          className="btn btn-outline-dark btn-sm"
+          onClick={() => setViewAll(!viewAll)}
+        >
+          {viewAll ? "Скрыть" : "Показать"} дополнительные поля
+        </button>
+      </div>
 
-        <Filter searchParams={props.searchParams} />
-        <table className="table table-bordered table-striped">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    {/* <th>Фото</th> */}
-                    <th>Наименование</th>
-                    <th>Заметки</th>
-                    <th>Код</th>
-                    {props.shops.map(shop => <th key={shop.id + 'a'} className="text-nowrap">Р.Ц. {shop.shopName}</th>)}
-                    {props.shops.map(shop => <th key={shop.id + 'b'} className="text-nowrap" > К - во {shop.shopName}</th>)}
-                    {viewAll && <>
-                        <th className="text-nowrap">Закуп. цена</th>
-                        <th className="text-nowrap">Себестоимость</th>
-                    </>}
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                {props.productsFull.map(product => <tr key={product.id}>
-                    <td>
-                        <Link href={`/products/view/${product.id}`} >{product.id}</Link>
-                    </td>
-                    {/* <td>
+      <Filter searchParams={props.searchParams} />
+      <table className="table table-bordered table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            {/* <th>Фото</th> */}
+            <th>Наименование</th>
+            <th>Заметки</th>
+            <th>Код</th>
+            {props.shops.map((shop) => (
+              <th key={shop.id + "a"} className="text-nowrap">
+                Р.Ц. {shop.shopName}
+              </th>
+            ))}
+            {props.shops.map((shop) => (
+              <th key={shop.id + "b"} className="text-nowrap">
+                {" "}
+                К - во {shop.shopName}
+              </th>
+            ))}
+            {viewAll && (
+              <>
+                <th className="text-nowrap">Закуп. цена</th>
+                <th className="text-nowrap">Себестоимость</th>
+              </>
+            )}
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.productsFull.map((product) => (
+            <tr key={product.id}>
+              <td>
+                <Link href={`/products/view/${product.id}`}>{product.id}</Link>
+              </td>
+              {/* <td>
                         {product.images.map(image => <Image
                             key={image.id}
                             loader={() => image.name}
@@ -62,86 +81,103 @@ export default function ViewProducts(props: {
                             height={0}
                             style={{ width: "auto", height: "auto", marginBottom: 5, cursor: "pointer", }} />)}
                     </td> */}
-                    <td><span style={{ color: product.color }}>{product.name}</span></td>
-                    <td>{product.note}</td>
-                    <td>{product.code}</td>
-                    <>
-                        {product.retailPrices.map((retailPriceObj, i) => {
-                            const costPrice = createPriceWithMarkup(
-                                product.purchase_price,
-                                retailPriceObj.idCostPriceType,
-                                retailPriceObj.costPriceValue
-                            );
-                            const retailPrice = createPriceWithMarkup(
-                                costPrice,
-                                retailPriceObj.retailPriceType,
-                                retailPriceObj.retailPriceValue
-                            );
-                            return <td key={i}>
-                                <Price value={retailPrice} />
-                            </td>
-                        })}
-                    </>
-                    <>
-                        {product.stock.map(stockObj => {
-                            return <td key={stockObj.id}>
-                                {stockObj.count}
-                            </td>
-                        })}
-                    </>
-                    {viewAll && <>
-                        <td>
-                            <Price value={product.purchase_price} />
-                        </td>
-                        <td>{(() => {
-                            const value = getCostPriceNumFromObj(product.purchase_price, {
-                                type: product.idCostPriceType,
-                                value: product.costPriceValue
-                            });
-                            return <Price value={value} />
-                        })()}</td>
-                    </>}
-                    <td style={{ whiteSpace: "nowrap" }}>
-                        <div className="d-flex">
-                            {/* <pre>{JSON.stringify(product, null, 2)}</pre> */}
-                            <SaleForm productFull={product} shops={props.shops}
-
-                                retailPrices={product.retailPrices.map(retailPriceObj => {
-                                    const costPrice = createPriceWithMarkup(
-                                        product.purchase_price,
-                                        retailPriceObj.idCostPriceType,
-                                        retailPriceObj.costPriceValue
-                                    );
-                                    const retailPrice = createPriceWithMarkup(
-                                        costPrice,
-                                        retailPriceObj.retailPriceType,
-                                        retailPriceObj.retailPriceValue
-                                    );
-                                    // const retailPriceValue = getRetailPriceNumFromObj(
-                                    //     product.purchase_price,
-                                    //     retailPriceObj, {
-                                    //     type: product.idCostPriceType,
-                                    //     value: product.costPriceValue
-                                    // });
-
-                                    return {
-                                        idShop: retailPriceObj.idShop,
-                                        sum: retailPrice
-                                    }
-
-                                    // return <td key={retailPriceObj.id}>
-                                    //     <Price value={retailPriceValue} />
-                                    // </td>
-                                })}
-                            />
-                            <EditProduct product={product} priceTypes={props.priceTypes}
-                                shops={props.shops}
-                            />
-                            <SendProductToArchive idProduct={product.id} productName={product.name} />
-                        </div>
+              <td>
+                <span style={{ color: product.color }}>{product.name}</span>
+              </td>
+              <td>{product.note}</td>
+              <td>{product.code}</td>
+              <>
+                {product.retailPrices.map((retailPriceObj, i) => {
+                  const costPrice = createPriceWithMarkup(
+                    product.purchase_price,
+                    retailPriceObj.idCostPriceType,
+                    retailPriceObj.costPriceValue
+                  );
+                  const retailPrice = createPriceWithMarkup(
+                    costPrice,
+                    retailPriceObj.retailPriceType,
+                    retailPriceObj.retailPriceValue
+                  );
+                  return (
+                    <td key={i}>
+                      <Price value={retailPrice} />
                     </td>
-                </tr>)}
-            </tbody>
-        </table >
+                  );
+                })}
+              </>
+              <>
+                {product.stock.map((stockObj) => {
+                  return <td key={stockObj.id}>{stockObj.count}</td>;
+                })}
+              </>
+              {viewAll && (
+                <>
+                  <td>
+                    <Price value={product.purchase_price} />
+                  </td>
+                  <td>
+                    {(() => {
+                      const value = getCostPriceNumFromObj(
+                        product.purchase_price,
+                        {
+                          type: product.idCostPriceType,
+                          value: product.costPriceValue,
+                        }
+                      );
+                      return <Price value={value} />;
+                    })()}
+                  </td>
+                </>
+              )}
+              <td style={{ whiteSpace: "nowrap" }}>
+                <div className="d-flex">
+                  {/* <pre>{JSON.stringify(product, null, 2)}</pre> */}
+                  <SaleForm
+                    productFull={product}
+                    shops={props.shops}
+                    retailPrices={product.retailPrices.map((retailPriceObj) => {
+                      const costPrice = createPriceWithMarkup(
+                        product.purchase_price,
+                        retailPriceObj.idCostPriceType,
+                        retailPriceObj.costPriceValue
+                      );
+                      const retailPrice = createPriceWithMarkup(
+                        costPrice,
+                        retailPriceObj.retailPriceType,
+                        retailPriceObj.retailPriceValue
+                      );
+                      // const retailPriceValue = getRetailPriceNumFromObj(
+                      //     product.purchase_price,
+                      //     retailPriceObj, {
+                      //     type: product.idCostPriceType,
+                      //     value: product.costPriceValue
+                      // });
+
+                      return {
+                        idShop: retailPriceObj.idShop,
+                        sum: retailPrice,
+                      };
+
+                      // return <td key={retailPriceObj.id}>
+                      //     <Price value={retailPriceValue} />
+                      // </td>
+                    })}
+                  />
+                  <EditProduct
+                    product={product}
+                    priceTypes={props.priceTypes}
+                    shops={props.shops}
+                  />
+                  <SendProductToArchive
+                    idProduct={product.id}
+                    productName={product.name}
+                  />
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
+  );
 }
