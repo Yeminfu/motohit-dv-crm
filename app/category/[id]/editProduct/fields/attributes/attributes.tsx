9f51679e8a8a6970b208dab722d1b0951e5e2aef
Yeminfu <yeminfu4319@gmail.com>
2024-11-08 08:35:21 +1000

    создал getProductAttributes

--- app/category/[id]/editProduct/fields/attributes/attributes.tsx
+++ app/category/[id]/editProduct/fields/attributes/attributes.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { Controller, useForm, useFormContext } from "react-hook-form";
 import getCategoryAttributes from "./utils/getCategoryAttributes";
 import ts_AttributeWithValues from "@/types/attributes/ts_attributesWithValues";
+import getProductAttributes from "./utils/getProductAttributes";
 
 export default function Attributes(props: {
   idProduct: number;
@@ -23,9 +24,13 @@ export default function Attributes(props: {
     ts_AttributeWithValues[]
   >([]);
 
-  // useEffect(() => {
-  //   console.log("idProduct", props.idProduct);
-  // }, [props.idProduct]);
+  useEffect(() => {
+    console.log("idProduct", props.idProduct);
+    (async () => {
+      const attributes = await getProductAttributes(props.idProduct);
+      console.log("attributes", attributes);
+    })();
+  }, [props.idProduct]);
 
   useEffect(() => {
     (async () => {
---
+++ app/category/[id]/editProduct/fields/attributes/utils/getProductAttributes.ts
@@ -0,0 +1,29 @@
+import ts_productAttributes from "@/types/products/ts_productAttributes";
+
+export default async function getProductAttributes(
+  idProduct: number
+): Promise<ts_productAttributes[]> {
+  try {
+    const response = await fetch("/api/attributes/getProductAttributes", {
+      method: "POST",
+      body: JSON.stringify({
+        idProduct,
+      }),
+    });
+    if (!response.ok) {
+      alert("err #f943mn");
+      throw new Error(`Ошибка: ${response.statusText}`);
+    }
+    const data = await response.json();
+
+    if (data.attributes) {
+      return data.attributes;
+    } else {
+      alert("err #d03dsmn");
+      return [];
+    }
+  } catch (error) {
+    alert("err #d0303mn");
+    return [];
+  }
+}
