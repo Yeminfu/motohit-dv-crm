5c5c008393afdca1d51b5307b47554c12d016ad8
Yeminfu <yeminfu4319@gmail.com>
2024-11-13 00:56:09 +1000

    кодстайл

--- app/api/products/edit/[id]/utils/getIdAttributeByProductAndValue.ts
+++ app/api/products/edit/[id]/utils/getIdAttributeByProductAndValue.ts
@@ -5,18 +5,18 @@ export default async function getIdAttributeByProductAndValue(
   idAttributeValue: number
 ): Promise<number> {
   const sql = `
-  select (
-    select
-      vals.idAttribute
-    from chbfs_attr_prod_relation rel
-      inner join chbfs_attributes_values vals on vals.id = rel.idAttributeValue
-      inner join chbfs_attributes attr on attr.id = vals.idAttribute
-    where
-      idProduct = ?
-      and rel.idAttributeValue = ?
-    limit 1
-  )
-  as idAttribute;
+    select (
+      select
+        vals.idAttribute
+      from chbfs_attr_prod_relation rel
+        inner join chbfs_attributes_values vals on vals.id = rel.idAttributeValue
+        inner join chbfs_attributes attr on attr.id = vals.idAttribute
+      where
+        idProduct = ?
+        and rel.idAttributeValue = ?
+      limit 1
+    )
+    as idAttribute;
   `;
   const res = await dbWorker(sql, [idProduct, idAttributeValue]).then((x) => {
     return Number(x.pop().idAttribute);
