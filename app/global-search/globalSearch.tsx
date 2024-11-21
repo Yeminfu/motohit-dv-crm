1eb0c19c899f7d98f01c50f7c543b7b57613443e
Yeminfu <yeminfu4319@gmail.com>
2024-11-22 08:59:07 +1000

    создал GlobalSearch

---
+++ app/global-search/globalSearch.tsx
@@ -0,0 +1,9 @@
+import Filter from "./filter";
+
+export default function GlobalSearch() {
+  return (
+    <>
+      <Filter searchParams={{}} />
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { ts_categoriesWithIerarchy } from "@/types/categories/ts_categoriesWithI
 import AuthedLayout from "@/utils/authedLayout";
 import getCategoriesWithIerarchy from "@/utils/side-menu/getCategoriesWithIerarchy";
 import Link from "next/link";
+import GlobalSearch from "./global-search/globalSearch";
 
 export default async function Home() {
   const categoriesWithIerarchy = await getCategoriesWithIerarchy();
@@ -10,8 +11,11 @@ export default async function Home() {
     <main>
       <AuthedLayout title="Главная">
         <>
+          <GlobalSearch />
           <div style={{ marginLeft: "-10px" }}>
-            {categoriesWithIerarchy.map(category => <CategoryItem category={category} key={category.id} />)}
+            {categoriesWithIerarchy.map((category) => (
+              <CategoryItem category={category} key={category.id} />
+            ))}
           </div>
         </>
       </AuthedLayout>
@@ -19,16 +23,24 @@ export default async function Home() {
   );
 }
 
-
 function CategoryItem(props: { category: ts_categoriesWithIerarchy }) {
-  const children = props.category.children ?
-    props.category.children?.map(child => <CategoryItem category={child} key={child.id} />)
+  const children = props.category.children
+    ? props.category.children?.map((child) => (
+        <CategoryItem category={child} key={child.id} />
+      ))
     : null;
 
-  return <>
-    <div style={{ marginLeft: "10px" }}>
-      <Link className="btn btn-outline-dark d-block text-start mb-1" href={`/category/${props.category.id}`}>{props.category.category_name}</Link>
-      {children}
-    </div>
-  </>
+  return (
+    <>
+      <div style={{ marginLeft: "10px" }}>
+        <Link
+          className="btn btn-outline-dark d-block text-start mb-1"
+          href={`/category/${props.category.id}`}
+        >
+          {props.category.category_name}
+        </Link>
+        {children}
+      </div>
+    </>
+  );
 }
