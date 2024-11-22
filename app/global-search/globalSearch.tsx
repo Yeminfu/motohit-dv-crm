9cb4585c15ede4f130fdeb6df421c15193452c28
Yeminfu <yeminfu4319@gmail.com>
2024-11-23 09:34:03 +1000

    сделал запрос товаров

--- app/global-search/globalSearch.tsx
+++ app/global-search/globalSearch.tsx
@@ -1,12 +1,15 @@
-import ts_categoryFilter from "#types/ts_categoryFilter.js";
+import ts_categoryFilter from "#types/ts_categoryFilter.ts";
 import Filter from "./filter";
+import getProductsFull from "./utils/getProductsFull";
 
-export default function GlobalSearch(props: {
+export default async function GlobalSearch(props: {
   searchParams: ts_categoryFilter;
 }) {
+  const productsFull = await getProductsFull(props.searchParams);
   return (
     <>
       <Filter searchParams={props.searchParams} />
+      <pre>{JSON.stringify({ productsFull }, null, 2)}</pre>
     </>
   );
 }
