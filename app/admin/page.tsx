996e17e11ddbbd30db907031009d08fa9c3c45ac
Yeminfu <yeminfu4319@gmail.com>
2024-11-24 11:19:49 +1000

    создал страницу admin

---
+++ app/admin/page.tsx
@@ -0,0 +1,9 @@
+import AuthedLayout from "#utils/authedLayout.tsx";
+
+export default async function Page() {
+  return (
+    <>
+      <AuthedLayout title="Панель администратора">...</AuthedLayout>
+    </>
+  );
+}
