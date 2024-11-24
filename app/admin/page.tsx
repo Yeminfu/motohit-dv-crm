7fd533e4f7d3f257e72e9b3486f5caa712e2c46c
Yeminfu <yeminfu4319@gmail.com>
2024-11-24 11:32:03 +1000

    вывел названия таблиц

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,36 @@
+import dbWorker from "#db/dbWorker.ts";
 import AuthedLayout from "#utils/authedLayout.tsx";
+import Link from "next/link";
 
 export default async function Page() {
+  const tables = await getDBTables();
   return (
     <>
-      <AuthedLayout title="Панель администратора">...</AuthedLayout>
+      <AuthedLayout title="Панель администратора">
+        <>
+          <h3>Таблицы</h3>
+          <table className="table table-bordered w-auto">
+            <tbody>
+              {tables.map((table) => (
+                <tr>
+                  <td>
+                    <Link
+                      className=""
+                      href={`/admin/tables/${table.Tables_in_motohit_dv_crm}`}
+                    >
+                      {table.Tables_in_motohit_dv_crm}
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      </AuthedLayout>
     </>
   );
+}
+
+async function getDBTables(): Promise<{ Tables_in_motohit_dv_crm: string }[]> {
+  return dbWorker(`show tables`, []);
 }
