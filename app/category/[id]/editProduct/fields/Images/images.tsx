c5f95badd2fb0613c22f89a746121efa7c8af700
Yeminfu <yeminfu4319@gmail.com>
2024-12-03 08:57:05 +1000

    убрал лишний код

--- app/category/[id]/editProduct/fields/Images/images.tsx
+++ app/category/[id]/editProduct/fields/Images/images.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import getImages from "./utils/getImages";
 import { useFieldArray, useFormContext } from "react-hook-form";
 import Image from "next/image";
 
 export default function Images(props: { idProduct: number }) {
-  const [images, setImages] = useState([]);
-  const { register, control, setValue, getValues } = useFormContext();
+  const { control } = useFormContext();
 
   const {
     fields: oldImages,
