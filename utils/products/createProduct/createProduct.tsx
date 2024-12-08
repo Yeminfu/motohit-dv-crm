0609f187165bb176996903b468b325449685fadc
Yeminfu <yeminfu4319@gmail.com>
2024-12-08 14:08:14 +1000

    up

--- utils/products/createProduct/createProduct.tsx
+++ utils/products/createProduct/createProduct.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState } from "react";
 import Modal from "../../modal/modal";
 import CreateProductForm from "./CreateProductForm";
@@ -7,32 +7,38 @@ import { PriceTypesFromDBInterface } from "@/types/products/priceTypesFromDBInte
 import tsAttributeWithValues from "@/types/attributes/ts_attributesWithValues";
 
 export default function CreateProduct(props: {
-    idCategory: number,
-    priceTypes: PriceTypesFromDBInterface[],
-    shops: ShopFromDB[]
-    attributesWithValues: tsAttributeWithValues[]
+  idCategory: number;
+  priceTypes: PriceTypesFromDBInterface[];
+  shops: ShopFromDB[];
+  attributesWithValues: tsAttributeWithValues[];
 }) {
-    const [isOpen, setIsOpen] = useState(false);
-    return <>
-        <button className="btn btn-outline-dark btn-sm" onClick={() => setIsOpen(!isOpen)}>
-            Создать товар
-        </button>
-        
-        <Modal
-            isOpen={isOpen}
-            title="Создать товар"
-            closeFn={() => {
-                setIsOpen(false);
-            }}
-        >
-            <CreateProductForm
-                idCategory={props.idCategory}
-                priceTypes={props.priceTypes}
-                shops={props.shops}
-                attributesWithValues={props.attributesWithValues}
-                closeFn={() => {
-                    setIsOpen(false);
-                }} />
-        </Modal>
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <button
+        className="btn btn-outline-dark btn-sm"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        Создать товар
+      </button>
+
+      <Modal
+        isOpen={isOpen}
+        title="Создать товар"
+        closeFn={() => {
+          setIsOpen(false);
+        }}
+      >
+        <CreateProductForm
+          idCategory={props.idCategory}
+          priceTypes={props.priceTypes}
+          shops={props.shops}
+          attributesWithValues={props.attributesWithValues}
+          closeFn={() => {
+            setIsOpen(false);
+          }}
+        />
+      </Modal>
     </>
+  );
 }
