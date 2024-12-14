7eb9a672f9768872ebc144098b90e1264309b3dc
Yeminfu <yeminfu4319@gmail.com>
2024-12-14 21:01:30 +1000

    up

--- app/admin/config/utils/insertClassToDB.ts
+++ app/admin/config/utils/insertClassToDB.ts
@@ -1,7 +1,10 @@
 import dbWorker from "#db/dbWorker.ts";
+import { ResultSetHeader } from "mysql2";
 import ts_class4create from "../types/ts_class4create";
 
-export default async function insertClassToDB(_class: ts_class4create) {
+export default async function insertClassToDB(
+  _class: ts_class4create
+): Promise<ResultSetHeader> {
   const res = await dbWorker(
     `
       insert into chbfs_sys$classes
