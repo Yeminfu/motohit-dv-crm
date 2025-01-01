860bcbf42249992aa2f3be6a5095dad8d23538e9
Yeminfu <yeminfu4319@gmail.com>
2025-01-01 20:46:21 +1000

    создал getConfigThree

--- app/admin/config/components/configThree/configThree.tsx
+++ app/admin/config/components/configThree/configThree.tsx
@@ -1,7 +1,15 @@
+import dbWorker from "#db/dbWorker2.ts";
+
 export default async function ConfigThree() {
+  const configThree = await getConfigThree();
   return (
     <>
       <div>ConfigThree</div>
+      <pre>{JSON.stringify(configThree, null, 2)}</pre>
     </>
   );
+}
+
+async function getConfigThree() {
+  return await dbWorker(`select * from chbfs_config`, []);
 }
