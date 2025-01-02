9f3f45c1fb221bbbe7392395c37255b7270c9108
Yeminfu <yeminfu4319@gmail.com>
2025-01-02 13:17:00 +1000

    сделал раскрывающийся список

--- app/admin/config/components/configThree/components/RecursiveView.tsx
+++ app/admin/config/components/configThree/components/RecursiveView.tsx
@@ -1,18 +1,36 @@
+"use client";
+
+import { useState } from "react";
 import ts_configWithChildren from "../types/ts_configWithChildren";
+import { toast } from "react-toastify";
 
 export default function RecursiveView(props: {
   config: ts_configWithChildren;
 }) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="card">
       <div className="card-header">
-        <strong>Conf: {props.config.name}</strong>
+        <div className="d-flex">
+          <strong
+            onClick={() => {
+              toast("открываем конфигу");
+            }}
+          >
+            Conf: {props.config.name}
+          </strong>
+          <div className="ms-2" onClick={() => setIsOpen(!isOpen)}>
+            открыть
+          </div>
+        </div>
       </div>
-      <div className="card-body">
-        {props.config.children.map((config) => (
-          <RecursiveView config={config} key={config.id} />
-        ))}
-      </div>
+      {isOpen && (
+        <div className="card-body">
+          {props.config.children.map((config) => (
+            <RecursiveView config={config} key={config.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
