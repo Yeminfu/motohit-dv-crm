c503a9ea7d225f0bcb657724dde4bdfd6bdebf1f
Yeminfu <yeminfu4319@gmail.com>
2025-01-05 14:42:34 +1000

    up

--- app/admin/api/procedures/create/route.ts
+++ app/admin/api/procedures/create/route.ts
@@ -4,31 +4,33 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   const procedure: ts_procedure4create = await request.json();
-  const resCreate = await DBCreateProcedure(procedure);
-  if (!resCreate.result) return NextResponse.json(resCreate);
 
-  const insertRes = await DBInsertProcedure(procedure);
-  return NextResponse.json(insertRes);
+  const resCreate = await DBCreateProcedure(procedure);
+  if (!resCreate.result) {
+    console.error("err #dsdf8", resCreate);
+    return NextResponse.json(resCreate);
+  }
+
+  return NextResponse.json(resCreate);
 }
 
 async function DBCreateProcedure(props: ts_procedure4create) {
-  return dbWorker(props.SQLString, []);
-}
-
-async function DBInsertProcedure(props: ts_procedure4create) {
   return dbWorker(
     `
-      insert into chbfs_sys$procedures
-      (
-        name,
-        title,
-        SQLString
-      )
-      values
-      (
-        ?,?,?
-      )
-    `,
+    ${props.SQLString};
+
+    insert into chbfs_sys$procedures
+    (
+      name,
+      title,
+      SQLString
+    )
+    values
+    (
+      ?,?,?
+    )
+
+  `,
     [props.name, props.title, props.SQLString]
   );
 }
