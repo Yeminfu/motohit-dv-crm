a2dac08ff64c205b0646ffdaf12017d7bebd65b3
Yeminfu <yeminfu4319@gmail.com>
2025-01-06 18:29:12 +1000

    пробросил colName

--- migrate/checkColumnIsExists/checkColumnIsExists.sql
+++ migrate/checkColumnIsExists/checkColumnIsExists.sql
@@ -3,19 +3,20 @@ drop function if exists checkColumnIsExists;
 
 DELIMITER //
 
-CREATE FUNCTION checkColumnIsExists(tableName varchar(250), b INT)
+CREATE FUNCTION checkColumnIsExists(tableName varchar(250), colName varchar(250))
 RETURNS INT
 DETERMINISTIC
 BEGIN
     SET @count = 0;
     SET @tableName = tableName;
+    SET @colName = colName;
 
     SELECT count(1) 
     INTO @count
     FROM information_schema.COLUMNS 
     where
     TABLE_NAME = @tableName
-    and COLUMN_NAME = 'id';
+    and COLUMN_NAME = @colName;
 
     return @count;
 END //
