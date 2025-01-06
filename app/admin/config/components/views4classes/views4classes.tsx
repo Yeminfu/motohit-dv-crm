83e80007778acb4c1b3c6b1ed96027f16b8fd9f6
Yeminfu <yeminfu4319@gmail.com>
2025-01-06 12:29:11 +1000

    создал Views4classes

--- app/admin/config/components/views4classes/views4classes.tsx
+++ app/admin/config/components/views4classes/views4classes.tsx
@@ -1,3 +1,15 @@
+import dbWorker from "#db/dbWorker2.ts";
+
 export default async function Views4classes() {
-  return <>Views4classes</>;
+  const views = await getViews();
+  return (
+    <>
+      Views4classes
+      <pre>{JSON.stringify(views)}</pre>
+    </>
+  );
+}
+
+async function getViews() {
+  return dbWorker(`select * from chbfs_sys$views`, []);
 }
