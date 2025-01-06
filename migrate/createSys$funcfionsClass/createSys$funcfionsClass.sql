533b7c2a832d00aa15997c966a98fe6af6810898
Yeminfu <yeminfu4319@gmail.com>
2025-01-06 13:54:41 +1000

    создал createSys$functionsClass

---
+++ migrate/createSys$funcfionsClass/createSys$funcfionsClass.sql
@@ -0,0 +1,31 @@
+drop procedure if exists createSys$functionsClass;
+
+DELIMITER $$
+create procedure createSys$functionsClass (
+  in value varchar(250)
+) 
+begin
+
+  set @value = value;
+
+  drop table if exists huyomboyom_sys$functions;
+
+  create table if not exists huyomboyom_sys$functions (
+      id int auto_increment primary key,
+      created_date datetime default current_timestamp,
+      name varchar(250) not null unique,
+      title varchar(250) not null,
+      description text,
+      idConfig int not null
+  );
+
+  insert into huyomboyom_sys$functions
+  (name,title,idConfig)
+  values
+  ('huyomboyom_sys$functions','Скалярные функции',1);
+
+
+end $$
+DELIMITER ;
+
+--call createSys$functionsClass(1)
