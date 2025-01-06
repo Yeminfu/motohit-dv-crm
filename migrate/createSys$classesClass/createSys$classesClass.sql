003649eddc3c72da716fb1b15f20a86118e6b2d7
Yeminfu <yeminfu4319@gmail.com>
2025-01-06 21:09:42 +1000

    up

--- migrate/createSys$classesClass/createSys$classesClass.sql
+++ migrate/createSys$classesClass/createSys$classesClass.sql
@@ -23,5 +23,65 @@ begin
   ('huyomboyom_sys$classes','Базовые классы',1);
 
 
+ 
+
+
+
+
+
+
+/*
+  drop table if exists fieldsStrings;
+  CREATE TABLE fieldsStrings (
+    fieldName varchar(255) not null,
+    params varchar(255) not null
+  );
+  INSERT INTO fieldsStrings (fieldName, params) VALUES 
+
+    ('id','int auto_increment primary key'),
+    ('created_date','datetime default current_timestamp'),
+    ('name','varchar(250) not null unique'),
+    ('title','varchar(250) not null'),
+    ('description','text'),
+    ('idConfig','int not null');
+
+  select 
+    -- concat(
+    --   '',
+    --   fieldName,
+    --   params
+    -- ),1
+    '' as command
+    ,fieldName
+    ,params
+  from fieldsStrings;
+  drop table fieldsStrings;
+
+
+  
+  INSERT INTO users (id, name, email)
+  VALUES (1, 'Иван', 'ivan@example.com')
+  ON DUPLICATE KEY UPDATE
+  name = VALUES(name), email = VALUES(email);
+*/
+
+
+
+
+
+
+/*
+  if checkColumnIsExists('huyomboyom_sys$classes','id') <> 1 
+   then
+     ALTER TABLE huyomboyom_sys$classes
+     ADD COLUMN id int auto_increment primary key;
+  end if;
+
+  if checkColumnIsExists('huyomboyom_sys$classes','created_date') <> 1 
+   then
+     ALTER TABLE huyomboyom_sys$classes
+     ADD COLUMN created_date datetime default current_timestamp;
+  end if;
+*/
 end $$
 DELIMITER ;
