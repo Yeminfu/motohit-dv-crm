195caa9054ca0e3aa7476de2a7de2b62a5fb45c4
Yeminfu <yeminfu4319@gmail.com>
2025-01-07 01:24:55 +1000

    вынес ts_viewFromDB

--- app/admin/config/components/views4classes/views4classes.tsx
+++ app/admin/config/components/views4classes/views4classes.tsx
@@ -1,12 +1,42 @@
+import Link from "next/link";
+import ts_viewFromDB from "../../types/ts_viewFromDB";
 import Create from "./components/create";
 import getViews from "./utils/getViews";
 
 export default async function Views4classes() {
-  const views = await getViews();
+  const viewsRes = await getViews();
   return (
     <>
       <Create />
-      <pre>{JSON.stringify(views)}</pre>
+      {(() => {
+        if (viewsRes.error) {
+          return <>err #ks93lsk</>;
+        }
+        if (!viewsRes.result) {
+          return <>err #jdsa93</>;
+        }
+        const views: ts_viewFromDB[] = viewsRes.result;
+
+        if (views)
+          return (
+            <table className="table table-bordered table-striped w-auto">
+              <tbody>
+                {views.map((view) => (
+                  <tr key={view.name} title={JSON.stringify(view, null, 2)}>
+                    <td>
+                      <Link href={`/admin/scalarFunctions/${view.name}`}>
+                        {view.name}
+                      </Link>
+                    </td>
+                    <td>{view.title}</td>
+                    <td>{view.description}</td>
+                    <td>{view.idClass}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          );
+      })()}
     </>
   );
 }
