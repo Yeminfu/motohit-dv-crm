baa918ab48bf68c576b86f2a50d0fd26e76ece82
Yeminfu <yeminfu4319@gmail.com>
2025-01-07 14:29:44 +1000

    up

---
+++ app/admin/views/[id]/components/viewOutput/components/consoleForm.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import ViewSQLResult from "#app/admin/sqlConsole/components/viewSQLResult/viewSQLResult.tsx";
+// import onSubmit from "#app/products/create/form/onSubmit.js";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+export default function ConsoleForm(props: { sql: string }) {
+  const { register, handleSubmit } = useForm<{ sql: string }>({
+    defaultValues: {
+      sql: props.sql,
+    },
+  });
+  const [state, setState] = useState<{ result: any | undefined }>();
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit(async (values) => {
+          const res = await onSubmit(values);
+          setState(res);
+        })}
+      >
+        <>
+          <textarea
+            rows={10}
+            {...register("sql", { required: true })}
+            className="form-control "
+            autoComplete="off"
+          />
+          <div className="mt-2">
+            <button className="btn btn-dark btn-sm">Go</button>
+          </div>
+        </>
+      </form>
+
+      <div className="mt-2">
+        <div className="shadow p-2">
+          {(() => {
+            if (!state) return <>введите запрос</>;
+            if (!state.result?.length)
+              return (
+                <>
+                  <strong>err #kfsdf8</strong>
+                  <pre>{JSON.stringify(state, null, 2)}</pre>
+                </>
+              );
+
+            if (state.result)
+              return (
+                <>
+                  <ViewSQLResult SQLResult={state.result} />
+                  {/* <pre>{JSON.stringify(["state", state.result], null, 2)}</pre> */}
+                </>
+              );
+            return (
+              <pre>{JSON.stringify({ err: "#kdsa83", state }, null, 2)}</pre>
+            );
+          })()}
+        </div>
+      </div>
+    </>
+  );
+}
+
+async function onSubmit(values: { sql: string }) {
+  try {
+    const response = await fetch(`/admin/api/sql`, {
+      method: "post",
+      body: JSON.stringify(values),
+    });
+    if (!response.ok) {
+      throw new Error(`Ошибка HTTP: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log("data", data);
+
+    return data;
+  } catch (error: any) {
+    alert("err #kds83m");
+  }
+}
--- app/admin/views/[id]/components/viewOutput/viewOutput.tsx
+++ app/admin/views/[id]/components/viewOutput/viewOutput.tsx
@@ -1,13 +1,21 @@
 import ts_view4edit from "#app/admin/views/types/ts_view4edit.ts";
+import ConsoleForm from "./components/consoleForm";
 
 export default function ViewOutput(props: ts_view4edit) {
+  const sql = `
+    select
+     *
+    from ${props.name}
+    limit 100
+    order by id desc
+  `;
   return (
     <>
       <div className="card">
         <div className="card-header">Вывод данных для</div>
         <div className="card-body"></div>
       </div>
-      ViewOutput #{props.id}
+      <ConsoleForm sql={sql} />
     </>
   );
 }
