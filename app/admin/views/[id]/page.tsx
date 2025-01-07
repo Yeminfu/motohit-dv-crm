2300ac7454387060db557aee7d59084965b6980b
Yeminfu <yeminfu4319@gmail.com>
2025-01-07 12:27:23 +1000

    создал edit view

--- app/admin/views/[id]/page.tsx
+++ app/admin/views/[id]/page.tsx
@@ -1,4 +1,5 @@
 import AuthedLayout from "#utils/authedLayout.tsx";
+import Edit from "./components/edit";
 import getView from "./utils/getView";
 
 export default async function Page(params: { params: { id: string } }) {
@@ -7,7 +8,10 @@ export default async function Page(params: { params: { id: string } }) {
   return (
     <>
       <AuthedLayout title="Представление (view)">
-        <pre>{JSON.stringify(view, null, 2)}</pre>
+        {(() => {
+          if (!view) return <>err #kdsk3m</>;
+          return <Edit {...view} />;
+        })()}
       </AuthedLayout>
     </>
   );
