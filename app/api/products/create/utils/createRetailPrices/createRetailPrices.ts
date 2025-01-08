8fa9a91bbee49684a931a40b4a0d4ad42e0e73eb
Yeminfu <yeminfu4319@gmail.com>
2025-01-08 12:20:30 +1000

    добавил хранимку в createRetailPrices

--- app/api/products/create/utils/createRetailPrices/createRetailPrices.ts
+++ app/api/products/create/utils/createRetailPrices/createRetailPrices.ts
@@ -5,23 +5,20 @@ import { ResultSetHeader } from "mysql2";
 export default async function createRetailPrices(
   idProduct: number,
   retail_price: RetailPriceFromDB[]
-): Promise<ResultSetHeader> {
-  const sql = `
-  insert into ${process.env.TABLE_PREFIX}_retail_prices
-  (
-    idPriceType,
-    priceValue,
-    idProduct,
-    idShop
-  )
-  values
-    ${retail_price.map(() => "(?,?,?,?)")}
-  `;
+): Promise<ResultSetHeader[]> {
+  const results = [];
 
-  const values = retail_price
-    .map((v) => [v.idPriceType, v.priceValue, idProduct, v.idShop])
-    .flat();
+  for (let index = 0; index < retail_price.length; index++) {
+    const v = retail_price[index];
+    const sql = `call createProductRetailPrice(?,?,?,?)`;
+    const result = await dbWorker(sql, [
+      v.idPriceType,
+      v.priceValue,
+      idProduct,
+      v.idShop,
+    ]);
+    results.push(result);
+  }
 
-  const res = await dbWorker(sql, values);
-  return res;
+  return results;
 }
