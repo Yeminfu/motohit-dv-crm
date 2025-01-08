ac036c9ef2cc9952eacf6a120b9e75742843b08a
Yeminfu <yeminfu4319@gmail.com>
2025-01-08 13:01:05 +1000

    добавил createProductImage

--- utils/products/saveImage/insertImageToDB.ts
+++ utils/products/saveImage/insertImageToDB.ts
@@ -5,16 +5,7 @@ export default async function insertImageToDB(
   filename: string,
   idProduct: number
 ): Promise<ResultSetHeader> {
-  const sql = `
-    insert into ${process.env.TABLE_PREFIX}_products_images
-    (
-      name, idProduct
-    )
-    values
-    (
-      ?,?
-    )
-  `;
+  const sql = `call createProductImage(?,?)`;
   const res = await dbWorker(sql, [filename, idProduct]);
   return res;
 }
