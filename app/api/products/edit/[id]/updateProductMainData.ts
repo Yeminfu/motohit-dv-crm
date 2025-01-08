75e3a61faa98d40efbdb10b4f136d5baadc5195f
Yeminfu <yeminfu4319@gmail.com>
2025-01-08 14:36:23 +1000

    добавил процедуру editProduct

--- app/api/products/edit/[id]/updateProductMainData.ts
+++ app/api/products/edit/[id]/updateProductMainData.ts
@@ -5,32 +5,16 @@ export default async function updateProductMainData(
   productData: ProductFromDB,
   idProduct: number
 ) {
-  const res = await dbWorker(
-    `
-    update ${process.env.TABLE_PREFIX}_products
-    set
-      name = ?,
-      note = ?,
-      idCostPriceType = ?,
-      idCategory = ?,
-      costPriceValue = ?,
-      purchase_price = ?,
-      color = ?,
-      code = ?
-    where
-      id = ?
-  `,
-    [
-      productData.name,
-      productData.note,
-      productData.idCostPriceType,
-      productData.idCategory,
-      productData.costPriceValue,
-      productData.purchase_price,
-      productData.color,
-      productData.code,
-      idProduct,
-    ]
-  );
+  const res = await dbWorker(`call editProduct(?,?,?,?,?,?,?,?,?)`, [
+    idProduct,
+    productData.name,
+    productData.note,
+    productData.idCostPriceType,
+    productData.idCategory,
+    productData.costPriceValue,
+    productData.purchase_price,
+    productData.color,
+    productData.code,
+  ]);
   return res;
 }
