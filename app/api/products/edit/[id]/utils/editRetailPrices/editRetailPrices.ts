ab7c0b023c107acfb0dcdbedc8fe0beef1c8f3d4
Yeminfu <yeminfu4319@gmail.com>
2025-01-08 15:03:16 +1000

    добавил процедуру updateRetailPrice

--- app/api/products/edit/[id]/utils/editRetailPrices/editRetailPrices.ts
+++ app/api/products/edit/[id]/utils/editRetailPrices/editRetailPrices.ts
@@ -1,6 +1,5 @@
 import { RetailPriceFromDB } from "#types/products/retailPriceFromDB.ts";
 import addHistoryEntry from "#utils/history/addHistoryEntry.ts";
-import insertRetailPrice from "./insertRetailPrice";
 import updateRetailPrice from "./updateRetailPrice";
 
 export default async function editRetailPrices(
@@ -8,18 +7,10 @@ export default async function editRetailPrices(
 ) {
   for (let index = 0; index < retail_price.length; index++) {
     const retailPriceObj = retail_price[index];
-    if (retailPriceObj.idRecord) {
-      const updRetailPriceRes = await updateRetailPrice(retailPriceObj);
-      await addHistoryEntry("updateProductRetailPrice", {
-        retailPriceObj,
-        updRetailPriceRes,
-      });
-    } else {
-      const insertRetailPriceRes = await insertRetailPrice(retailPriceObj);
-      await addHistoryEntry("insertProductRetailPrice", {
-        retailPriceObj,
-        insertRetailPriceRes,
-      });
-    }
+    const updRetailPriceRes = await updateRetailPrice(retailPriceObj);
+    await addHistoryEntry("updateProductRetailPrice", {
+      retailPriceObj,
+      updRetailPriceRes,
+    });
   }
 }
--- app/api/products/edit/[id]/utils/editRetailPrices/updateRetailPrice.ts
+++ app/api/products/edit/[id]/utils/editRetailPrices/updateRetailPrice.ts
@@ -1,27 +1,18 @@
 import dbConnection from "@/db/connect";
 import { RetailPriceFromDB } from "@/types/products/retailPriceFromDB";
 
-export default async function updateRetailPrice(retailPriceObj: RetailPriceFromDB) {
-
+export default async function updateRetailPrice(
+  retailPriceObj: RetailPriceFromDB
+) {
   const connection = await dbConnection();
   const updateRes = await connection
-    .query(
-      `
-        update ${process.env.TABLE_PREFIX}_retail_prices
-        set
-          idPriceType = ?,
-          priceValue = ?,
-          idProduct = ?,
-          idShop = ?
-        where id  = ?`,
-      [
-        retailPriceObj.idPriceType,
-        retailPriceObj.priceValue,
-        retailPriceObj.idProduct,
-        retailPriceObj.idShop,
-        retailPriceObj.idRecord
-      ]
-    )
+    .query(`call updateRetailPrice(?,?,?,?,?)`, [
+      retailPriceObj.idProduct,
+      retailPriceObj.idShop,
+      retailPriceObj.idPriceType,
+      retailPriceObj.priceValue,
+      retailPriceObj.idRecord,
+    ])
     .then(([x]: any) => {
       return x;
     })
