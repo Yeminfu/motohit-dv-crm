445d5921e06ae0b94cf1fd90d2bbdab6fdd90af5
Yeminfu <yeminfu4319@gmail.com>
2025-01-11 09:05:11 +1000

    сделал ссылку на ключ

--- app/admin/config/components/keys/keys.tsx
+++ app/admin/config/components/keys/keys.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import getDBKeys from "../../utils/getDBKeys";
 import Create from "./components/create/create";
 import DeleteKeyBtn from "./components/deleteKeyBtn/deleteKeyBtn";
@@ -8,24 +9,32 @@ export default async function Keys() {
   return (
     <>
       <Create />
-      {/* <table className="table table-bordered w-auto">
+      <table className="table table-bordered w-auto">
         <thead>
           <tr>
-            {(() => {
+            <td>name</td>
+            {/* {(() => {
               const dbKey = dbKeys[0];
               const keysFromObject = Object.keys(dbKey);
               return keysFromObject.map((key) => <th key={key}>{key}</th>);
-            })()}
+            })()} */}
           </tr>
         </thead>
         <tbody>
           {dbKeys.map((keyObject) => {
             return (
-              <tr key={keyObject.COLUMN_NAME}>
-                {Object.values(keyObject).map((value) => (
-                  <td key={value}>{value}</td>
-                ))}
+              <tr key={keyObject.id}>
                 <td>
+                  <Link href={`/admin/keys/${keyObject.name}`}>
+                    {keyObject.name}
+                  </Link>
+                </td>
+                {/* {Object.values(keyObject).map((value) => (
+                  <td key={value}>
+                    <Link href={`/admin/keys/${value}`}>{value}</Link>
+                  </td>
+                ))} */}
+                {/* <td>
                   <div
                     className="btn"
                     title={JSON.stringify(keyObject, null, 2)}
@@ -39,12 +48,12 @@ export default async function Keys() {
                   >
                     удалить ключ
                   </div>
-                </td>
+                </td> */}
               </tr>
             );
           })}
         </tbody>
-      </table> */}
+      </table>
     </>
   );
 }
--- app/admin/config/types/ts_keyFromDB.ts
+++ app/admin/config/types/ts_keyFromDB.ts
@@ -1,7 +1,7 @@
 export default interface ts_keyFromDB {
-  TABLE_NAME: string;
-  COLUMN_NAME: string;
-  CONSTRAINT_NAME: string;
-  REFERENCED_TABLE_NAME: string;
-  REFERENCED_COLUMN_NAME: string;
+  id: number;
+  name: string;
+  SQLString: string;
+  idConfig: number;
+  title: string;
 }
