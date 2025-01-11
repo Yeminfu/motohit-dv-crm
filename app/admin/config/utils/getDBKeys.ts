7b6f5a5e49ee06c9cf0029e80931c96c04153a83
Yeminfu <yeminfu4319@gmail.com>
2025-01-12 08:26:20 +1000

    вернул как было

--- app/admin/config/utils/getDBKeys.ts
+++ app/admin/config/utils/getDBKeys.ts
@@ -4,23 +4,16 @@ import ts_keyFromDB from "../types/ts_keyFromDB";
 export default async function getDBKeys(): Promise<ts_keyFromDB[]> {
   return dbWorker(
     `
-      select * from chbfs_sys$keys
+      select 
+      id,
+      name,
+      SQLString,
+      description,
+      idConfig,
+      title,
+      tableName,
+      from chbfs_sys$keys
     `,
     []
   );
-
-  /*
-    "CONSTRAINT_CATALOG": "def",
-    "CONSTRAINT_SCHEMA": "motohit_dv_crm",
-    "CONSTRAINT_NAME": "chbfs_attr_prod_relation_ibfk_1",
-    "TABLE_CATALOG": "def",
-    "TABLE_SCHEMA": "motohit_dv_crm",
-    "TABLE_NAME": "chbfs_attr_prod_relation",
-    "COLUMN_NAME": "idAttributeValue",
-    "ORDINAL_POSITION": 1,
-    "POSITION_IN_UNIQUE_CONSTRAINT": 1,
-    "REFERENCED_TABLE_SCHEMA": "motohit_dv_crm",
-    "REFERENCED_TABLE_NAME": "chbfs_attributes_values",
-    "REFERENCED_COLUMN_NAME": "id"
-  */
 }
