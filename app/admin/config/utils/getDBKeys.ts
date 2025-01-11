6f8c0381cf0dce59decfe4ed8c516cca711d0b00
Yeminfu <yeminfu4319@gmail.com>
2025-01-12 08:27:45 +1000

    gjabrcbk запрос в getDBKeys

--- app/admin/config/utils/getDBKeys.ts
+++ app/admin/config/utils/getDBKeys.ts
@@ -5,13 +5,13 @@ export default async function getDBKeys(): Promise<ts_keyFromDB[]> {
   return dbWorker(
     `
       select 
-      id,
-      name,
-      SQLString,
-      description,
-      idConfig,
-      title,
-      tableName,
+        id,
+        name,
+        SQLString,
+        description,
+        idConfig,
+        title,
+        tableName
       from chbfs_sys$keys
     `,
     []
