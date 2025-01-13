bfc10ebbf15f13dbe02ac97007b1009d5e884504
Yeminfu <yeminfu4319@gmail.com>
2025-01-13 11:06:33 +1000

    сделал ссылку на скачивание файла

--- app/admin/components/downloadSQLDump/downloadSQLDump.tsx
+++ app/admin/components/downloadSQLDump/downloadSQLDump.tsx
@@ -1,20 +1,17 @@
 "use client";
 
+import Link from "next/link";
+
 export default function DownloadSQLDump() {
   return (
     <>
-      <button
-        onClick={() => {
-          getDump();
-        }}
+      <Link
         className="btn btn-outline-dark"
+        href="/admin/api/get-dump"
+        target="_blank"
       >
         getDump
-      </button>
+      </Link>
     </>
   );
-}
-
-async function getDump() {
-  await fetch("/admin/api/get-dump", { method: "post" });
 }
