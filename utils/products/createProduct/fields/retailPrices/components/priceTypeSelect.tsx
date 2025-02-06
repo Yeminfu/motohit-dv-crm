6bd8ca5a3d3cb30955001eee8adda06e2a67ae9e
Yeminfu <yeminfu4319@gmail.com>
2025-02-06 18:43:16 +1000

    добавил PriceTypeSelect

---
+++ utils/products/createProduct/fields/retailPrices/components/priceTypeSelect.tsx
@@ -0,0 +1,36 @@
+import { PriceTypesFromDBInterface } from "#types/products/priceTypesFromDBInterface.ts";
+import { useEffect } from "react";
+import { useFormContext } from "react-hook-form";
+
+export default function PriceTypeSelect(props: {
+  fieldName: string;
+  priceTypes: PriceTypesFromDBInterface[];
+}) {
+  const { register, setValue } = useFormContext();
+  useEffect(() => {
+    setValue(props.fieldName, "3");
+  }, []);
+  return (
+    <>
+      <select
+        {...(() => {
+          return register(
+            //@ts-ignore
+            props.fieldName,
+            {
+              required: true,
+            }
+          );
+        })()}
+        className="form-select"
+        autoComplete="off"
+      >
+        {props.priceTypes.map((priceType) => (
+          <option value={priceType.id} key={priceType.id}>
+            {priceType.priceType}
+          </option>
+        ))}
+      </select>
+    </>
+  );
+}
