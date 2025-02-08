b849022db2d85f38f847eb13ec26eb39c586fc7a
Yeminfu <yeminfu4319@gmail.com>
2025-02-08 11:25:11 +1000

    убрал лишний компонент

--- app/attributes/category/[id]/page.tsx
+++ app/attributes/category/[id]/page.tsx
@@ -40,18 +40,10 @@ export default async function Page(a: { params: { id: string } }) {
                   {attribute.id}
                 </Link>
               </td>
-              <td>
-                {attribute.attribute_name} <EditField />
-              </td>
-              <td>
-                {attribute.view_in_filter} <EditField />
-              </td>
-              <td>
-                {attribute.isOpenInFilter} <EditField />
-              </td>
-              <td>
-                {attribute.is_main} <EditField />
-              </td>
+              <td>{attribute.attribute_name}</td>
+              <td>{attribute.view_in_filter}</td>
+              <td>{attribute.isOpenInFilter}</td>
+              <td>{attribute.is_main}</td>
               <td>
                 <ViewAttributeValues attributeValues={attribute.values} />
               </td>
@@ -84,33 +76,4 @@ interface ts_attributeValue {
   created_date: Date;
   created_by: number;
   idAttribute: number;
-}
-
-function EditField() {
-  return (
-    <>
-      <Pencil />
-    </>
-  );
-}
-
-function Pencil() {
-  return (
-    <>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="currentColor"
-        className="bi bi-pencil-square"
-        viewBox="0 0 16 16"
-      >
-        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"></path>
-        <path
-          fill-rule="evenodd"
-          d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
-        ></path>
-      </svg>
-    </>
-  );
 }
