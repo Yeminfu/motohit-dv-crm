d5bfa644f0d812f8b78ab85b9187b5c2a58fd7f9
Yeminfu <yeminfu4319@gmail.com>
2025-02-08 11:23:49 +1000

    сделал switcher

--- app/attributes/category/[id]/components/viewAttributeValues.tsx
+++ app/attributes/category/[id]/components/viewAttributeValues.tsx
@@ -1,20 +1,28 @@
 "use client";
 import ts_attributeValue from "#types/attributes/ts_attributeValue.ts";
+import { useState } from "react";
 
 export default function ViewAttributeValues(props: {
   attributeValues: ts_attributeValue[];
 }) {
+  const [view, setView] = useState(false);
+  const switchView = () => setView(!view);
   return (
     <>
-      <table className="table">
-        <tbody>
-          {props.attributeValues.map((attributeValue) => (
-            <tr key={attributeValue.id}>
-              <td>{attributeValue.value_name}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <button className="btn btn-outline-dark btn-sm" onClick={switchView}>
+        {view ? "скрыть" : "показать"} значения
+      </button>
+      {view && (
+        <table className="table mt-2">
+          <tbody>
+            {props.attributeValues.map((attributeValue) => (
+              <tr key={attributeValue.id}>
+                <td>{attributeValue.value_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   );
 }
