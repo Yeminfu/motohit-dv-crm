f73a3e93e4c946ad922ae9aeab1bfc20d7fbc1ab
Yeminfu <yeminfu4319@gmail.com>
2025-02-13 21:07:52 +1000

    добавил создавшего пользователя в запрос

--- app/api/attributesValues/create/route.ts
+++ app/api/attributesValues/create/route.ts
@@ -1,10 +1,26 @@
-import dbWorker from "@/db/dbWorker2";
 import ts_inputs from "@/app/attributes/get/[id]/components/attributeValueCreator/types/ts_inputs";
 import { NextRequest, NextResponse } from "next/server";
 import createAttributeValue from "./utils/createAttributeValue";
+import getUserByToken from "@/utils/users/getUserByToken";
 
 export async function POST(request: NextRequest) {
   const data: ts_inputs = await request.json();
-  const res = await createAttributeValue(data)
+
+  const { cookies } = request;
+  const authToken = String(cookies.get("auth")?.value);
+  if (!authToken) {
+    return NextResponse.json({
+      error: "#ksdfsdf934i"
+    })
+  }
+
+  const user = await getUserByToken(authToken);
+  if (!user) {
+    return NextResponse.json({
+      error: "#lfds94jmw"
+    })
+  }
+
+  const res = await createAttributeValue(data, user.id)
   return NextResponse.json(res)
 }
--- app/api/attributesValues/create/utils/createAttributeValue.ts
+++ app/api/attributesValues/create/utils/createAttributeValue.ts
@@ -1,20 +1,21 @@
 import dbWorker from "@/db/dbWorker2";
 import ts_inputs from "@/app/attributes/get/[id]/components/attributeValueCreator/types/ts_inputs";
 
-export default async function createAttributeValue(props: ts_inputs) {
+export default async function createAttributeValue(props: ts_inputs, created_by: number) {
   const sql = `
     insert into chbfs_attributes_values
     (
       idAttribute,
-      value_name
+      value_name,
+      created_by
     )
     values
     (
-      ?,?
+      ?,?,?
     );
     select last_insert_id();
   `;
 
-  const res = await dbWorker(sql, [props.idAttribute, props.value_name]);
+  const res = await dbWorker(sql, [props.idAttribute, props.value_name, created_by]);
   return res;
 }
