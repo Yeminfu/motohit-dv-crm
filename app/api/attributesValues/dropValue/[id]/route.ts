afe6a2ffe39c94888b1a739535cc1bc7fa096a1e
Yeminfu <yeminfu4319@gmail.com>
2025-02-13 21:26:51 +1000

    удалил кусок миграции

--- app/api/attributesValues/dropValue/[id]/route.ts
+++ app/api/attributesValues/dropValue/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import deleteAttributeValue from "./utils/deleteAttributeValue";
 
-export async function POST(_: '_', params: { params: { id: string } }) {
+export async function POST(request: any, params: { params: { id: string } }) {
   const res = await deleteAttributeValue(Number(params.params.id));
   // if (!res.) {
   //   console.log('error #ksdff94', res);
