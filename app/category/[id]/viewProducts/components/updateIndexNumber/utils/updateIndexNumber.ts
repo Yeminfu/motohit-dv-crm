344c38ab34bca0a49a6af6b7e69517506be127aa
Yeminfu <yeminfu4319@gmail.com>
2025-02-17 08:35:18 +1000

    отправил данные на роут, сделал роут

--- app/category/[id]/viewProducts/components/updateIndexNumber/utils/updateIndexNumber.ts
+++ app/category/[id]/viewProducts/components/updateIndexNumber/utils/updateIndexNumber.ts
@@ -1,7 +1,29 @@
+import { toast } from "react-toastify";
+
 export default async function updateIndexNumber(
   idProduct: number,
   idNextProduct: number
 ) {
   console.log({ idProduct, idNextProduct });
+
+
+  try {
+    const response = await fetch("/api/products/update-index-number", {
+      method: "post",
+      body: JSON.stringify({ idProduct, idNextProduct })
+    });
+
+
+    if (!response.ok) {
+      throw new Error(`Ошибка HTTP: ${response.status} ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log('Полученные данные:', data);
+
+  } catch (error) {
+    toast.error('Ошибка #jsdf942');
+    alert('Ошибка #jsdf942' + JSON.stringify(error, null, 2))
+  }
 
 }
