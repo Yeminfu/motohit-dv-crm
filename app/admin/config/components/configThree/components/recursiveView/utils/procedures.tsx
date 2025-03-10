0986969a2815a055852599c380507cb562bc22f0
Yeminfu <yeminfu4319@gmail.com>
2025-03-10 16:55:24 +1000

    вывел процедуры тарлицей

--- app/admin/config/components/configThree/components/recursiveView/utils/procedures.tsx
+++ app/admin/config/components/configThree/components/recursiveView/utils/procedures.tsx
@@ -3,20 +3,23 @@
 import { useEffect, useState } from "react";
 
 export default function Procedures(props: { idConfig: number }) {
-  const [procedures, setProcedures] = useState();
+  const [procedures, setProcedures] = useState<
+    {
+      name: string;
+      title: string;
+      id: number;
+      SQLString: string;
+    }[]
+  >();
   useEffect(() => {
     fetch("/admin/api/procedures/getProceduresByIdConfig", {
       method: "post",
       body: JSON.stringify(props),
     })
-      .then((x) => x.text())
-      //@ts-ignore
+      .then((x) => x.json())
       .then((x) => {
-        console.log("xxxxxxxx", x);
-
-        const dataFromJSON = JSON.parse(x.replace(/\\n/gim, "\n"));
-        if (dataFromJSON.procedures) {
-          setProcedures(dataFromJSON.procedures);
+        if (x.procedures) {
+          setProcedures(x.procedures);
         } else {
           alert(JSON.stringify(x, null, 2));
         }
@@ -25,7 +28,38 @@ export default function Procedures(props: { idConfig: number }) {
   return (
     <>
       client procedures {props.idConfig}
-      <pre>{JSON.stringify(procedures, null, 2)}</pre>
+      {procedures && (
+        <table className="table table-striped table-bordered">
+          <thead
+            style={{
+              background: "#eee",
+              position: "sticky" /* Делаем заголовки липкими */,
+              top: 0 /* Устанавливаем верхнюю границу */,
+              backgroundColor: "white" /* Фон заголовка */,
+              zIndex: 1 /* Убедитесь, что заголовок выше других элементов */,
+            }}
+          >
+            <tr>
+              <th>id</th>
+              <th>name</th>
+              <th>title</th>
+              <th>SQLString</th>
+            </tr>
+          </thead>
+          <tbody>
+            {procedures.map((procedure) => (
+              <tr key={procedure.id}>
+                <td>{procedure.id}</td>
+                <td>{procedure.name}</td>
+                <td>{procedure.title}</td>
+                <td>
+                  <pre>{procedure.SQLString}</pre>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   );
 }
