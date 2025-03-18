06ec99def022c124e90a7f433e782063a24b1e06
Yeminfu <yeminfu4319@gmail.com>
2025-03-18 13:36:14 +1000

    вывел категорию таблицей

--- app/page.tsx
+++ app/page.tsx
@@ -32,9 +32,18 @@ export default async function Home(params: {
               </div>
               <div className="card-body">
                 <div style={{ marginLeft: "-10px" }}>
-                  {categoriesWithIerarchy.map((category) => (
-                    <CategoryItem category={category} key={category.id} />
-                  ))}
+                  <table className="table table-bordered">
+                    <tbody>
+                      {categoriesWithIerarchy.map((category) => (
+                        <tr key={category.id}>
+                          <td>
+                            <CategoryItem category={category} />
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+
+                  </table>
                 </div>
               </div>
             </div>
@@ -47,23 +56,47 @@ export default async function Home(params: {
 
 async function CategoryItem(props: { category: ts_categoriesWithIerarchy }) {
   const children = props.category.children
-    ? props.category.children?.map((child) => (
-        <CategoryItem category={child} key={child.id} />
-      ))
+    ? <table className="table">
+      {props.category.children?.map((child) => (
+        <tr key={child.id} >
+          <td>
+            <CategoryItem category={child} />
+          </td>
+        </tr>
+      ))}
+    </table>
     : null;
 
   const isInShop = await checkCategoryIsInShop(props.category.id);
 
   return (
     <>
+      <table className="table table-bordered w-auto" >
+        <thead>
+          <tr>
+            <th>Категория</th>
+            <th>Потомки</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <Link
+                style={{ maxWidth: "250px" }}
+                className="btn btn-outline-dark d-block text-start mb-1"
+                href={`/category/${props.category.id}`}
+              >
+                {props.category.category_name}
+              </Link>
+
+            </td>
+            <td>
+              {children}
+            </td>
+          </tr>
+        </tbody>
+      </table>
       <div style={{ marginLeft: "10px" }}>
-        <Link
-          className="btn btn-outline-dark d-block text-start mb-1"
-          href={`/category/${props.category.id}`}
-        >
-          {props.category.category_name}
-        </Link>
-        {children}
       </div>
     </>
   );
