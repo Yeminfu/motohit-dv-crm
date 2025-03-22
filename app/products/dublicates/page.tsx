7cfb961e3d7aefe15494a83610218ce6c525fb69
Yeminfu <yeminfu4319@gmail.com>
2025-03-22 16:51:21 +1000

    создал страницу дублей

---
+++ app/products/dublicates/page.tsx
@@ -0,0 +1,11 @@
+import AuthedLayout from "@/utils/authedLayout";
+
+export default async function Page() {
+  return <>
+    <AuthedLayout title="дубли ебаные">
+      <>
+        asd
+      </>
+    </AuthedLayout>
+  </>
+}
