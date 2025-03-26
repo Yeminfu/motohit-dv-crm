be27eaf7e1ad9bbe3d4b6e17c485615ce4ceaad4
Yeminfu <yeminfu4319@gmail.com>
2025-03-27 08:23:54 +1000

    отступы

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -4,34 +4,34 @@ import CreateUserForm from "./createUserForm";
 import getShops from "../../utils/getShops";
 
 export default async function Page() {
-    const users = await getAllUsers();
-    const shops = await getShops()
-    return <>
-        <AuthedLayout title={"Пользователи"}>
-            <>
-                <table className="table w-auto">
-                    <thead>
-                        <tr>
-                            <th>id</th>
-                            <th>Имя</th>
-                            <th>Телеграм</th>
-                            <th>Регистрация подтверждена</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users.map(user => <tr key={user.id}>
-                            <td>{user.id - 1}</td>
-                            <td>{user.name}</td>
-                            <td>{user.telegram_username}</td>
-                            <td>{user.tg_chat_id ? 'да' : 'нет'}</td>
-                        </tr>)}
+  const users = await getAllUsers();
+  const shops = await getShops()
+  return <>
+    <AuthedLayout title={"Пользователи"}>
+      <>
+        <table className="table w-auto">
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>Имя</th>
+              <th>Телеграм</th>
+              <th>Регистрация подтверждена</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user => <tr key={user.id}>
+              <td>{user.id - 1}</td>
+              <td>{user.name}</td>
+              <td>{user.telegram_username}</td>
+              <td>{user.tg_chat_id ? 'да' : 'нет'}</td>
+            </tr>)}
 
-                    </tbody>
-                </table>
-                <div className="mt-4">
-                    <CreateUserForm shops={shops} />
-                </div>
-            </>
-        </AuthedLayout>
-    </>
+          </tbody>
+        </table>
+        <div className="mt-4">
+          <CreateUserForm shops={shops} />
+        </div>
+      </>
+    </AuthedLayout>
+  </>
 }
