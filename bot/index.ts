d468114dd5294ad28831173022e7fbbe4366b060
Yeminfu <yeminfu4319@gmail.com>
2025-04-26 16:41:17 +1000

    up

--- bot/index.ts
+++ bot/index.ts
@@ -5,14 +5,17 @@ import sendMessage from './src/telegramApi/sendMessage/sendMessage';
 import fs from "fs";
 import xlsx from 'node-xlsx';
 import sendReportSalesPerDay from './src/reports/sendReportSalesPerDay/sendReportSalesPerDay';
+import dayjs from 'dayjs';
+
 
 console.log('hello');
 const token = process.env.TOKEN;
 console.log(token);
 
-
 setInterval(() => {
-
-  sendReportSalesPerDay();
-
-}, 5000);
+  const nowHour = dayjs().format('HH');
+  console.log(nowHour);
+  if (nowHour === '16') {
+    sendReportSalesPerDay();
+  }
+}, 1000);
